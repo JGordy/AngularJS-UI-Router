1c7bc13a7329c4af8ecd058b16953feb91478d1f
Joseph Gordy <jgordy2424@gmail.com>
2018-03-16 17:06:59 -0400

    Finished tutorial on UI-Router

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     url: '/home',
     templateUrl: 'partial-home.html'
   })
+
   //nested list with custom controller
   .state('home.list', {
     url: '/list',
@@ -18,14 +19,52 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
       $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
     }
   })
+
   //nested list with just some random string data
   .state('home.paragraph', {
     url: '/paragraph',
     template: 'I could sure use a drink right now'
   })
+
   // About page and multiple named views --------
   .state('about', {
-    // TODO for later
-  })
+    url: '/about',
+    views: {
+      // the main template will be places here (relative named)
+      '': { templateUrl: 'partial-about.html'},
+
+      // the child views will be defined here (absolute named)
+      'columnOne@about': {template: 'Look I am a column!'},
+
+      // for column two, we'll define a seperate controller
+      'columnTwo@about': {
+        templateUrl: 'table-data.html',
+        controller: 'scotchController'
+      }
+    }
+  });
+
+}); // closes $routerApp.config
+
+
+
+// defining a scotch controller we use up in the about state
+routerApp.controller('scotchController', function($scope) {
+  $scope.message = 'test';
+
+  $scope.scotches = [
+    {
+      name: 'Macallan 12',
+      price: 50
+    },
+    {
+      name: 'Chivas Regal Royal Salute',
+      price: 10000
+    },
+    {
+      name: 'Glenfiddich 1937',
+      price: 20000
+    }
+  ];
 
 })
